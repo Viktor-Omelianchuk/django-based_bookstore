ed9ef9a926c47ba0c6ed799cde34d46588988342
Viktor Omelianchuk <omelianchuk.viktor@gmail.com>
2020-10-05 15:51:37 +0300

    debag_toolbar

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -5,11 +5,17 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # Django admin
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # User management
-    path('accounts/', include('allauth.urls')),
+    path("accounts/", include("allauth.urls")),
     # Local apps
-    path('', include('pages.urls')),
-    path('books/', include('books.urls')),
-    path('orders/', include('orders.urls')),
+    path("", include("pages.urls")),
+    path("books/", include("books.urls")),
+    path("orders/", include("orders.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls)), ] + urlpatterns
